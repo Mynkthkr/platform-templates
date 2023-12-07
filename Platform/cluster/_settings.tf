674b31188b33cf2ef65d96a5a6481e6f066dd4ac
kumarma <mayank.kumar@tabcorp.com.au>
2023-12-07 15:59:24 +0530

    Added cluster

--- Platform/cluster/_settings.tf
+++ Platform/cluster/_settings.tf
@@ -7,7 +7,7 @@ locals {
   #env       = yamldecode(file("${path.module}/config.yml"))
  # common    = local.env["common"]
   # env_space = yamldecode(file("config.yml"))
-  env_space = yamldecode(file("${path.module}../../../platform-config/platform/cluster/config.yml"))
+  env_space = yamldecode(file("${path.module}/../../../platform-config/platform/cluster/config.yml"))
   # env_space = yamldecode(file("${path.module}/../../platform-config/platform/cluster/config.yml"))
 
   workspace = local.env_space["workspace"][terraform.workspace]
